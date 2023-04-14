211a8df0f3bae696d46eb1d487a11837af6ee73b
Denys Batrak <baden.i.ua@gmail.com>
2023-04-14 22:52:16 +0300

    Fix some errors.

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, createContext } from "react";
+import React, { useState, useEffect } from "react";
 import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { app, googleProvider } from "./firebase";
 
