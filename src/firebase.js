c94d18f0c0a50f2a1fe651007616617b6a36ca65
Denys Batrak <baden.i.ua@gmail.com>
2023-04-15 05:30:27 +0300

    Fix prod build errors.

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -50,4 +50,6 @@ class Firebase {
   }
 }
 
-export default new Firebase();
+const firebase = new Firebase();
+
+export default firebase;
